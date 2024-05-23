5d6d7039c8518d35949ab5a80f751706402f41f9
chikuu9800 <163832594+chikuu9800@users.noreply.github.com>
2024-05-23 12:02:46 +0530

    db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise'
 
 const connection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "crude_opretion"
+    host: "sql12.freesqldatabase.com",
+    user: "sql12708538",
+    password: "ACKE5CvBJp",
+    database: "sql12708538"
 })
 
 
