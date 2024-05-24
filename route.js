b977fceed5a919147de19cb11a30c4558cc50442
chikuu9800 <163832594+chikuu9800@users.noreply.github.com>
2024-05-24 12:13:29 +0530

    route.js

--- route.js
+++ route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { addfunction, deletefunction, showbyid, showfunction, updatefunction } from "./controllers.js";
+import { addfunction, deletefunction, showbyid, showfunction, updatefunction } from "./controllersUsers.js";
 
 const routes = express.Router()
 
 .get('/',showfunction)
 .post('/input',addfunction)
-.delete('/delete:id',deletefunction)
-.put('/update:id',updatefunction)
-.get('/byid:id',showbyid)
+.delete('/delete:email',deletefunction)
+.put('/update:email',updatefunction)
+.get('/byid:email',showbyid)
 
 export default routes;
